83eab8764baf8e4a216aec09e0d41fa30562f9d3
taoran <1252026067@qq.com>
2019-08-21 17:01:50 +0800

    新增3题

---
+++ app/src/main/java/com/example/leetcode/tree/TreeNode.java
@@ -0,0 +1,14 @@
+package com.example.leetcode.tree;
+
+/**
+ * 二叉树节点
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
