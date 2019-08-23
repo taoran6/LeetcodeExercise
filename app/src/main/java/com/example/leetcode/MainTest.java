f9fc5e3e17757ec6074754336d1bd73a00c3df5a
taoran <1252026067@qq.com>
2019-08-23 17:27:43 +0800

    新增2题

--- app/src/main/java/com/example/leetcode/MainTest.java
+++ app/src/main/java/com/example/leetcode/MainTest.java
@@ -1,5 +1,6 @@
 package com.example.leetcode;
 
+import com.example.leetcode.stack.RemoveOuterParentheses;
 import com.example.leetcode.string.CountAndSay;
 import com.example.leetcode.string.Palindrome;
 
@@ -8,6 +9,6 @@ import com.example.leetcode.string.Palindrome;
  */
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println(new Palindrome().isPalindrome("OP"));
+        System.out.println(new RemoveOuterParentheses().removeOuterParentheses("(()())(())"));
     }
 }
