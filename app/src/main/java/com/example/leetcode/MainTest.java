7c6c123ce9d4a02692c8db4758b2cfdde5acf8a2
taoran <1252026067@qq.com>
2019-08-30 14:49:07 +0800

    新增3题

--- app/src/main/java/com/example/leetcode/MainTest.java
+++ app/src/main/java/com/example/leetcode/MainTest.java
@@ -1,18 +1,14 @@
 package com.example.leetcode;
 
 import com.example.leetcode.array.FindDisappearedNumbers;
-import com.example.leetcode.compute.HammingDistance;
-import com.example.leetcode.tree.CreateNode;
-import com.example.leetcode.tree.PathSum;
-import com.example.leetcode.tree.TreeNode;
+import com.example.leetcode.array.Rotate;
+import com.example.leetcode.string.DefangIPaddr;
 
 /**
  * 测试用
  */
 public class MainTest {
     public static void main(String[] args) {
-        int[] nums = new int[]{4,3,2,7,8,2,3,1};
-        System.out.println(new FindDisappearedNumbers().findDisappearedNumbers(nums));
-
+        new Rotate().rotate3(new int[]{-2,-100,-1,99}, 2);
     }
 }
