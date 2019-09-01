6a7e6592dc72c1f17492afee786474c36a4f3f54
taoran <1252026067@qq.com>
2019-09-01 19:57:17 +0800

    新增1题

--- app/src/main/java/com/example/leetcode/array/Duplicate.java
+++ app/src/main/java/com/example/leetcode/array/Duplicate.java
@@ -37,4 +37,46 @@ public class Duplicate {
         }
         return false;
     }
+
+    /**
+     * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+     *
+     * 示例 1:
+     *
+     * 输入: s = "anagram", t = "nagaram"
+     * 输出: true
+     * 示例 2:
+     *
+     * 输入: s = "rat", t = "car"
+     * 输出: false
+     * 说明:
+     * 你可以假设字符串只包含小写字母。
+     *
+     * 进阶:
+     * 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+     *
+     *
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/valid-anagram
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * 方法一：排序
+     * 方法二：hash表
+     * 方法三：只包含小写字母，使用长度为26的数组替代hash表
+     * 三种方法都比较简单，就只写一种啦
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram(String s, String t) {
+        //注意这里可以提前判断
+        if(s.length() != t.length()) return false;
+
+        char[] sa = s.toCharArray();
+        char[] ta = t.toCharArray();
+        Arrays.sort(sa);
+        Arrays.sort(ta);
+        return Arrays.equals(sa, ta);
+    }
 }
