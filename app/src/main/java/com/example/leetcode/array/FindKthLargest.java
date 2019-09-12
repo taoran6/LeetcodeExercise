b2aa2694195de46a012574a7c1088b2dbd670544
taoran <1252026067@qq.com>
2019-09-12 17:50:59 +0800

    新增1题

---
+++ app/src/main/java/com/example/leetcode/array/FindKthLargest.java
@@ -0,0 +1,60 @@
+package com.example.leetcode.array;
+
+import java.util.PriorityQueue;
+
+/**
+ * 数组中的第K个最大元素
+ */
+public class FindKthLargest {
+    /**
+     * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个
+     * 不同的元素。
+     *
+     * 示例 1:
+     *
+     * 输入: [3,2,1,5,6,4] 和 k = 2
+     * 输出: 5
+     * 示例 2:
+     *
+     * 输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+     * 输出: 4
+     * 说明:
+     *
+     * 你可以假设 k 总是有效的，且 1 ≤ k ≤ 数组的长度。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * 方法一：先排序，取第k位放，略
+     *
+     * 方法二：使用堆
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(queue.size() < k) queue.add(nums[i]);
+            else {
+                if(queue.peek() < nums[i]) {
+                    queue.remove();
+                    queue.add(nums[i]);
+                }
+            }
+        }
+        return queue.peek();
+    }
+
+    /**
+     * TODO 方法三：快速选择
+     *
+     * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/solution/shu-zu-zhong-de-di-kge-zui-da-yuan-su-by-leetcode/
+     */
+    public int findKthLargest3(int[] nums, int k) {
+        return 0;
+    }
+
+}
