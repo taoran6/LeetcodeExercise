4a9a1501d3cbc37357c19390617df1ebb8a8d6ff
taoran <1252026067@qq.com>
2019-10-15 18:10:57 +0800

    新增2题

--- app/src/main/java/com/example/leetcode/DP/UniquePaths.java
+++ app/src/main/java/com/example/leetcode/DP/UniquePaths.java
@@ -70,4 +70,45 @@ public class UniquePaths {
         return nums[n - 1];
     }
 
+    /**
+     * 最小路径和
+     *
+     * 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+     *
+     * 说明：每次只能向下或者向右移动一步。
+     *
+     * 示例:
+     *
+     * 输入:
+     * [
+     *   [1,3,1],
+     *   [1,5,1],
+     *   [4,2,1]
+     * ]
+     * 输出: 7
+     * 解释: 因为路径 1→3→1→1→1 的总和最小。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/minimum-path-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        if(m == 0) return 0;
+        int n = grid[0].length;
+        if(n == 0) return 0;
+
+        int[][] min = new int[m][n];
+        min[0][0] = grid[0][0];
+        for(int i = 1; i < n; i++) min[0][i] = min[0][i - 1] + grid[0][i];
+        for(int j =1; j < m; j++) min[j][0] = min[j -1][0] + grid[j][0];
+
+        for(int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                min[i][j] = Math.min(min[i - 1][j], min[i][j - 1]) + grid[i][j];
+            }
+        }
+        return min[m - 1][n - 1];
+    }
+
 }
---
+++ app/src/main/java/com/example/leetcode/graph/NumIslands.java
@@ -0,0 +1,98 @@
+package com.example.leetcode.graph;
+
+/**
+ * 岛屿数量
+ */
+public class NumIslands {
+    /**
+     * 给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向
+     * 或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+     *
+     * 示例 1:
+     *
+     * 输入:
+     * 11110
+     * 11010
+     * 11000
+     * 00000
+     *
+     * 输出: 1
+     * 示例 2:
+     *
+     * 输入:
+     * 11000
+     * 11000
+     * 00100
+     * 00011
+     *
+     * 输出: 3
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/number-of-islands
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        int ans = 0;
+        int m = grid.length;
+        if(m == 0) return 0;
+        int n = grid[0].length;
+        if(n == 0) return 0;
+
+        boolean[][] isVisited = new boolean[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1' && !isVisited[i][j]) {
+                    ans ++;
+                    markGrid(i, j, grid, isVisited);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private void markGrid(int i, int j, char[][] grid, boolean[][] isVisited) {
+        if(grid[i][j] == '1' && !isVisited[i][j]) {
+            isVisited[i][j] = true;
+            if(i > 0) markGrid(i - 1, j, grid, isVisited);
+            if(j > 0) markGrid(i, j -1, grid, isVisited);
+            if(i < grid.length - 1) markGrid(i + 1, j, grid, isVisited);
+            if(j < grid[0].length - 1) markGrid(i, j+ 1, grid, isVisited);
+        }
+    }
+
+    /**
+     * 方法二：优化，可以省去 boolean[][] isVisited 这个空间，用grid[i][j] = '0'
+     */
+    public int numIslands2(char[][] grid) {
+        int ans = 0;
+        int m = grid.length;
+        if(m == 0) return 0;
+        int n = grid[0].length;
+        if(n == 0) return 0;
+
+        boolean[][] isVisited = new boolean[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1') {
+                    ans ++;
+                    markGrid2(i, j, grid);
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    private void markGrid2(int i, int j, char[][] grid) {
+        if(grid[i][j] == '1') {
+            grid[i][j] = '0';
+            if(i > 0) markGrid2(i - 1, j, grid);
+            if(j > 0) markGrid2(i, j -1, grid);
+            if(i < grid.length - 1) markGrid2(i + 1, j, grid);
+            if(j < grid[0].length - 1) markGrid2(i, j+ 1, grid);
+        }
+    }
+}
