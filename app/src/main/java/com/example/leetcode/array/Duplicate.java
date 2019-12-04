219b3711ab60e5bfaf176acf05e6da47620e263a
taoran <1252026067@qq.com>
2019-12-04 16:21:42 +0800

    新增2题

--- app/src/main/java/com/example/leetcode/array/Duplicate.java
+++ app/src/main/java/com/example/leetcode/array/Duplicate.java
@@ -163,4 +163,43 @@ public class Duplicate {
         return false;
     }
 
+
+    /**
+     * 485. 最大连续1的个数
+     *
+     * 给定一个二进制数组， 计算其中最大连续1的个数。
+     *
+     * 示例 1:
+     *
+     * 输入: [1,1,0,1,1,1]
+     * 输出: 3
+     * 解释: 开头的两位和最后的三位都是连续1，所以最大连续1的个数是 3.
+     * 注意：
+     *
+     * 输入的数组只包含 0 和1。
+     * 输入数组的长度是正整数，且不超过 10,000。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/max-consecutive-ones
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int ans = 0;
+        int maxCur = 0;
+
+        for (int i : nums) {
+            if(i == 1){
+                maxCur ++;
+            } else {
+                ans = Math.max(ans, maxCur);
+                maxCur = 0;
+            }
+        }
+        ans = Math.max(ans, maxCur);
+        return ans;
+    }
+
 }
--- app/src/main/java/com/example/leetcode/array/SetZeroes.java
+++ app/src/main/java/com/example/leetcode/array/SetZeroes.java
@@ -116,4 +116,63 @@ public class SetZeroes {
             for(int i = 0; i < m; i++) matrix[i][0] = 0;
         }
     }
+
+    /**
+     * 1089. 复写零
+     *
+     * 给你一个长度固定的整数数组 arr，请你将该数组中出现的每个零都复写一遍，并将其余的元素向右平移。
+     *
+     * 注意：请不要在超过该数组长度的位置写入元素。
+     *
+     * 要求：请对输入的数组 就地 进行上述修改，不要从函数返回任何东西。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：[1,0,2,3,0,4,5,0]
+     * 输出：null
+     * 解释：调用函数后，输入的数组将被修改为：[1,0,0,2,3,0,0,4]
+     * 示例 2：
+     *
+     * 输入：[1,2,3]
+     * 输出：null
+     * 解释：调用函数后，输入的数组将被修改为：[1,2,3]
+     *  
+     *
+     * 提示：
+     *
+     * 1 <= arr.length <= 10000
+     * 0 <= arr[i] <= 9
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/duplicate-zeros
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public void duplicateZeros(int[] arr) {
+        // 统计需要复写0的个数
+        int count = 0;
+        int i = 0;
+        for (; i + count < arr.length; i++) {
+            if(arr[i] == 0) {
+                count++;
+            }
+        }
+
+        //双指针，从后往前
+        int left = arr.length - 1 - count;
+        int right = arr.length - 1;
+
+        //这里需要注意，最后一个0没有复写空间的情况
+        //比如[3,0,1,0,1] 复写后变成[3,0,0,1,0]，但是count是等于2的
+        if(i + count > arr.length) {
+            arr[right --] = 0;
+        }
+        while (left >=0 && left != right) {
+            if(arr[left] == 0) {
+                arr[right --] = 0;
+            }
+            arr[right --] = arr[left --];
+        }
+    }
 }
