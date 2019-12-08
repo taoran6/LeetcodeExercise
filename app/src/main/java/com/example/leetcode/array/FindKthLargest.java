568c49751dce93d7feb831c542a6f89b34b7e09b
taoran <1252026067@qq.com>
2019-12-08 20:34:40 +0800

    新增3题

--- app/src/main/java/com/example/leetcode/array/FindKthLargest.java
+++ app/src/main/java/com/example/leetcode/array/FindKthLargest.java
@@ -93,4 +93,64 @@ public class FindKthLargest {
         return low;
     }
 
+    /**
+     * 414. 第三大的数
+     *
+     * 给定一个非空数组，返回此数组中第三大的数。如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。
+     *
+     * 示例 1:
+     *
+     * 输入: [3, 2, 1]
+     *
+     * 输出: 1
+     *
+     * 解释: 第三大的数是 1.
+     * 示例 2:
+     *
+     * 输入: [1, 2]
+     *
+     * 输出: 2
+     *
+     * 解释: 第三大的数不存在, 所以返回最大的数 2 .
+     * 示例 3:
+     *
+     * 输入: [2, 2, 3, 1]
+     *
+     * 输出: 1
+     *
+     * 解释: 注意，要求返回第三大的数，是指第三大且唯一出现的数。
+     * 存在两个值为2的数，它们都排第二。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/third-maximum-number
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public int thirdMax(int[] nums) {
+        //这里使用long, 防止出现[1,2,-2147483648]的情况
+        long max1 = Long.MIN_VALUE;
+        long max2 = Long.MIN_VALUE;
+        long max3 = Long.MIN_VALUE;
+
+        for(int a : nums) {
+            if(a <= max3 || a == max1 || a == max2) continue;
+            // a > max3 且 a < max2
+            else if(a < max2) {
+                max3 = a;
+            }
+            // a > max2 且 a < max1
+            else if(a < max1) {
+                max3 = max2;
+                max2 = a;
+            }
+            // a > max1
+            else {
+                max3 = max2;
+                max2 = max1;
+                max1 = a;
+            }
+        }
+
+        return (int)(max3 != Long.MIN_VALUE ? max3 : max1);
+    }
+
 }
--- app/src/main/java/com/example/leetcode/search/FindPeakElement.java
+++ app/src/main/java/com/example/leetcode/search/FindPeakElement.java
@@ -57,4 +57,71 @@ public class FindPeakElement {
         }
         return left;
     }
+
+    /**
+     * 852. 山脉数组的峰顶索引
+     *
+     * 我们把符合下列属性的数组 A 称作山脉：
+     *
+     * A.length >= 3
+     * 存在 0 < i < A.length - 1 使得A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1]
+     * 给定一个确定为山脉的数组，返回任何满足 
+     * A[0] < A[1] < ... A[i-1] < A[i] > A[i+1] > ... > A[A.length - 1] 的 i 的值。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：[0,1,0]
+     * 输出：1
+     * 示例 2：
+     *
+     * 输入：[0,2,1,0]
+     * 输出：1
+     *  
+     *
+     * 提示：
+     *
+     * 3 <= A.length <= 10000
+     * 0 <= A[i] <= 10^6
+     * A 是如上定义的山脉
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/peak-index-in-a-mountain-array
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * O(n)复杂度的方法很简单，就略过啦
+     *
+     * 方法一：二分查找
+     */
+    public int peakIndexInMountainArray(int[] A) {
+        int left = 0;
+        int right = A.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            //由于给定的峰值一定存在，一定会走到这个分支
+            if(A[mid] > A[mid - 1] && A[mid] > A[mid + 1]) return mid;
+            if(A[mid] > A[mid - 1] && A[mid] < A[mid + 1]) left = mid;
+            else right = mid;
+        }
+        return left;
+    }
+
+    /**
+     * 方法二：另一种二分查找
+     *
+     * 只需要查找A[i] < A[i+1] 的点即可
+     */
+    public int peakIndexInMountainArray2(int[] A) {
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            int mi = lo + (hi - lo) / 2;
+            if (A[mi] < A[mi + 1])
+                lo = mi + 1;
+            else
+                hi = mi;
+        }
+        return lo;
+    }
 }
