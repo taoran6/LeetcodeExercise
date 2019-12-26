9a99e414c0950909eca20bdf06b1fd6fdabce2c1
taoran <1252026067@qq.com>
2019-12-26 16:11:00 +0800

    新增2题

--- app/src/main/java/com/example/leetcode/hash/WordPattern.java
+++ app/src/main/java/com/example/leetcode/hash/WordPattern.java
@@ -53,4 +53,40 @@ public class WordPattern {
         }
         return true;
     }
+
+    /**
+     * 383. 赎金信
+     *
+     * 给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串
+     * magazines里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+     *
+     * (题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。)
+     *
+     * 注意：
+     *
+     * 你可以假设两个字符串均只含有小写字母。
+     *
+     * canConstruct("a", "b") -> false
+     * canConstruct("aa", "ab") -> false
+     * canConstruct("aa", "aab") -> true
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/ransom-note
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] aChars = new int[26];
+        int[] bChars = new int[26];
+        for (char c : ransomNote.toCharArray()) {
+            aChars[c - 'a'] ++;
+        }
+        for (char c : magazine.toCharArray()) {
+            bChars[c - 'a'] ++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if(aChars[i] > bChars[i]) return false;
+        }
+        return true;
+    }
 }
